c46bf259cff542864793c0a94c5953312ae02871
Rajibbepary <rajib635356@gmail.com>
2026-09-01 11:57:29 +0600

    next config js file update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        pathname: "**",
       },
     ],
-    unoptimized: true,
   },
 };
 
